6a9c64a1e731d854ad6b53873b43daadcc169f55
Ubuntu <ubuntu@ip-172-31-5-22.ec2.internal>
2021-11-24 18:20:25 +0000

    Created the basic_math.py and area_per_rect.py files. In the basic_math.py I used simple math operations to answer questions and in the area_per_rect.py file I found the area and perimeter of a rectangle

---
+++ area_per_rect.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Created by: Ina Tolo
+# Created on: Nov. 24, 2021
+# This program calculates and displays the area and perimeter of a
+# rectangle with length 9cm and width 4cm.
+
+def main():
+    print("For a rectangle that has dimensions")
+    print("9cm X 4cm")
+    print("")
+    print("Area")
+    print("= 9 * 4")
+    print("= {} cm^2". format(9*4))
+    print("")
+    print("Permimeter")
+    print("= 2 (9 + 4)")
+    print("= 2 (13) ")
+    print("= {} cm". format(2*(9+4)))
+
+
+if __name__ == "__main__":
+    main()
